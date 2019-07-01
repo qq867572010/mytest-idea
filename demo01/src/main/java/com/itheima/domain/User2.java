ed3a3076343078ef054779b38a5f688860e99aa2
ChenYH <woshicyh867572010@qq.com>
2019-07-01 17:45:00 +0800

    提交到本地仓库

--- demo01/src/main/java/com/itheima/domain/User2.java
+++ demo01/src/main/java/com/itheima/domain/User2.java
@@ -3,6 +3,12 @@ package com.itheima.domain;
 public class User2 {
 
     private Integer id;
-    private Integer money;
+    private Integer money1;
+    private Integer money2;
+    private Integer money3;
+    private Integer money4;
+    private Integer money5;
+    private Integer money6;
+    private Integer money7;
 
 }
